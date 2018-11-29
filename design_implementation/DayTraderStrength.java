fec2d9e3800058285b92869d9f80542d8983734c
Georges Nasrallah <georges19232@gmail.com>
2018-11-28 17:37:53 -0800

    fixed

--- design_implementation/DayTraderStrength.java
+++ design_implementation/DayTraderStrength.java
@@ -1,57 +1,58 @@
-package design_implementation;
+
+// package design_implementation;
 import java.util.Arrays;
 
 public class DayTraderStrength {
-	int algorithmAforStrength(int[] profits){
+	int algorithmAforStrength(int[] profits) {
 
-		int max = 0;
-		int sum = 0;
+		int strength = 0;
+		int performance = 0;
 		int n = profits.length;
-		for(int i = 0; i < n; i++) {
-		    for(int j = i; j <n; j++){
-		       sum = 0; 
-		        for(int k = i; k <=j; k++) {
-		            sum = sum + profits[k];        
-		            		if(max < sum) {
-		            			max = sum;
-		            		}
-		        }
-		    }
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) {
+				performance = 0;
+				for (int k = i; k <= j; k++) {
+					performance = performance + profits[k];
+					if (strength < performance) {
+						strength = performance;
+					}
+				}
+			}
 		}
-		return max;
+		return strength;
+	}
+
+	public int algorithmBforStrength(int[] profits) {
+		int[] performance = new int[profits.length + 1];
+		performance[0] = 0;
+
+		for (int i = 1; i < performance.length; i++) {
+			int prev = (performance[i - 1] + profits[i - 1]);
+			if (prev > profits[i - 1]) {
+				performance[i] = prev;
+			} else {
+				performance[i] = profits[i - 1];
+			}
 		}
-	
-    public int algorithmBforStrength(int [] profits){
-        int [] ar = new int[profits.length+1];
-        ar[0] = 0;
 
-        for (int i = 1; i < ar.length ; i++) {
-        		int prev = (ar[i-1]+profits[i-1]);
-        		if(prev > profits[i-1]) {
-        			ar[i] = prev;
-        		}else {
-        			ar[i] = profits[i-1];
-        		}
-        }
+		int strength = 0;
+		for (int x = 1; x < performance.length; x++) {
+			if (performance[x] > strength)
+				strength = performance[x];
+		}
 
-        int max = 0;
-        for (int x = 1; x <ar.length ; x++) {
-            if(ar[x] > max)
-                max = ar[x];
-        }
+		return strength;
+	}
 
-        return max;
-    }
+	public static void main(String[] args) {
+		int profits[] = new int[3000];
 
-    public static void main(String[] args) {
-        int profits[] = new int[3000];
-    		
-    		for(int i = 0; i < profits.length ; i++) {
-    		 profits[i] = (int) (-10 + Math.random()*20);
-    		}
-    		DayTraderStrength i = new DayTraderStrength();
-        System.out.println("dynamic  " + i.algorithmBforStrength(profits));
-        System.out.println("brute  " + i.algorithmAforStrength(profits));
+		for (int i = 0; i < profits.length; i++) {
+			profits[i] = (int) (-10 + Math.random() * 20);
+		}
+		DayTraderStrength i = new DayTraderStrength();
+		System.out.println("dynamic  " + i.algorithmBforStrength(profits));
+		System.out.println("brute  " + i.algorithmAforStrength(profits));
 
-    }
+	}
 }
